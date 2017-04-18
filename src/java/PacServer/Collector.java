b5d5356ff5d52ce6f0d2488e37f4e58d02b1d681
stanislous <stanislousvanderputt@gmail.com>
2017-04-18 16:10:21 +0530

    add dot format & player format methods

--- src/java/PacServer/Collector.java
+++ src/java/PacServer/Collector.java
@@ -1,10 +1,13 @@
 package PacServer;
 
 import java.util.Random;
+import org.json.simple.JSONObject;
 
 public class Collector {
     
     Random rand = new Random();
+    JSONObject obj = new JSONObject();
+    
     Collector(){ 
         
     Dots newDot1 = new Dots("R",rand.nextInt(44),rand.nextInt(44));
@@ -24,5 +27,13 @@ public class Collector {
     Players player2 = new Players("P2",6,0,44);
     Players player3 = new Players("P3",2,44,0);
     Players player4 = new Players("P4",8,44,44);
+    }
+    
+    public void DotFormet(){
+    
+    }
+    
+    public void PlayerFormat(){
+    
     }
 }
