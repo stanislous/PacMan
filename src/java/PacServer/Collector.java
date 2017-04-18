1835c133f25d49164418f9283bdacad9710711e4
stanislous <stanislousvanderputt@gmail.com>
2017-04-18 21:55:25 +0530

    Used loops for Dots and Players

--- src/java/PacServer/Collector.java
+++ src/java/PacServer/Collector.java
@@ -2,46 +2,58 @@ package PacServer;
 
 import java.util.Random;
 import org.json.simple.*;
+import java.util.ArrayList;
 
 public class Collector {
-    
+     
     Random rand = new Random();
     static JSONObject obj = new JSONObject();
-    //Collection<JSONObject> items = new ArrayList<JSONObject>();
+    ArrayList<Dots> dot_list=new ArrayList<Dots>();
+    ArrayList<Players> player_list=new ArrayList<Players>();
     
-    Dots newDot1,newDot2,newDot3,newDot4,newDot5,newDot6,newDot7,newDot8,newDot9,newDot10,newDot11,newDot12;
-   
-    Players player1,player2,player3,player4;
     
-     Collector(){ 
+    Collector(){ 
         
-    newDot1 = new Dots("R",rand.nextInt(44),rand.nextInt(44));
-    newDot2 = new Dots("G",rand.nextInt(44),rand.nextInt(44));
-    newDot3 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-    newDot4 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-    newDot5 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-    newDot6 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-    newDot7 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-    newDot8 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-    newDot9 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-    newDot10 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-    newDot11 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-    newDot12 = new Dots("B",rand.nextInt(44),rand.nextInt(44));
-   
-    player1 = new Players("P1",5,0,0);
-    player2 = new Players("P2",6,0,44);
-    player3 = new Players("P3",2,44,0);
-    player4 = new Players("P4",8,44,44);
+        for (int i = 0; i < 12; i++) {
+            dot_list.add(new Dots(Color(), rand.nextInt(44), rand.nextInt(44)));
+        }
+        
+        for (int i = 1; i <= 4; i++) {
+            player_list.add(new Players(player(i), i, rand.nextInt(44), rand.nextInt(44)));
+            
+        }
+
     }
+    
+    public String Color(){
+        int color = rand.nextInt(2);
+        if (color == 0) {
+            return "R";
+        }else if (color == 1) {
+            return "G";
+        }else
+            return "B";   
+    }
+    public String player(int i){
+        if (i==1) {
+            return "P1";
+        }else if (i==2) {
+            return "P2";
+        }else if (i==3) {
+            return "P3";
+        }else
+            return "P4";
+    }
+    
     
     public void DotFormat(){
         
         JSONArray array1 = new JSONArray();
 //      JSONArray array2 = new JSONArray();
         
-        array1.add(newDot1.color);
-        array1.add(newDot1.x);
-        array1.add(newDot1.y);
+        array1.add(dot_list.get(1).color);
+        array1.add(dot_list.get(1).x);
+        array1.add(dot_list.get(1).y);
         
         
         obj.put("DOTS", array1);
